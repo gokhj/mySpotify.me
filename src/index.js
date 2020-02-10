fa5f82f46338f13ab9a0449746d25e3268c5151e
gokhj <gokhanarkan@gmail.com>
2020-02-10 21:57:53 +0000

    initializing firebase

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from 'firebase';
+import * as firebaseConfig from './firebase.json';
+
+firebase.initializeApp(firebaseConfig.default);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
